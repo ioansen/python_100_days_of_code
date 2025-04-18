28df052ecb3fcc05d1b9bc77002a398b70c139e7
ioanmvm <ioan@myvirtualmission.com>
2025-04-18 13:59:45 +0300

    tested git pipeline

---
+++ d1_print_input/band_namer.py
@@ -0,0 +1,4 @@
+print("We will create a band namer")
+city = input("what is your favourite city or the city you live in?\n")
+pet = input("what is your pet name?\n")
+print("There is the name for your next band:", city, pet)
